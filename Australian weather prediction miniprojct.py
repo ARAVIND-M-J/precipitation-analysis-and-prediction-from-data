723fcff078d598288cb2e90713388ff9465aac9f
ARAVIND-M-J <60753567+ARAVIND-M-J@users.noreply.github.com>
2020-02-08 11:12:15 +0530

    test commit

--- Australian weather prediction miniprojct.py
+++ Australian weather prediction miniprojct.py
@@ -131,17 +131,3 @@ from sklearn.neighbors import KNeighborsClassifier
 classifier= KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
 classifier.fit(X_train,y_train)
 y_pred_knn=classifier.predict(X_test)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
